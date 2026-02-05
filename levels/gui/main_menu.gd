0441168e73382ed58d8c4b5c99c443c3faacff16
jormungand_pc <Jormungan.dz@gmail.com>
2026-02-05 16:20:17 +0300

    add main menu ui

---
+++ levels/gui/main_menu.gd
@@ -0,0 +1,32 @@
+extends Control
+
+enum MENUSTATES {MENU, OPTION, LOAD}
+var ui_state = MENUSTATES.MENU
+
+
+func _on_settings_button_up() -> void:
+	ui_state = MENUSTATES.OPTION
+	$MenuContent.visible = false
+	$OptionMenu.visible = true
+
+func _on_load_button_up() -> void:
+	ui_state = MENUSTATES.LOAD
+	$MenuContent.visible = false
+	$LoadMenu.visible = true
+
+func _input(event: InputEvent) -> void:
+	if event.is_action_pressed("ui_cancel"):
+		match ui_state:
+			MENUSTATES.OPTION:
+				ui_state = MENUSTATES.MENU
+				$MenuContent.visible = true
+				$OptionMenu.visible = false
+			MENUSTATES.LOAD:
+				ui_state = MENUSTATES.MENU
+				$MenuContent.visible = true
+				$LoadMenu.visible = false
+			
+
+
+func _on_exit_button_up() -> void:
+	get_tree().quit()
